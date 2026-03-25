4ad435d1ff34a0ddd584f0e1282d3530fe9670c9
umem1125 <chaerulumam1125@gmail.com>
2026-03-25 18:44:04 +0700

    setup deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        URL::forceHttps(config('app.force_https'));
     }
 
     /**
